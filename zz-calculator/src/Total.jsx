35fcea14847fb76da5d1144505cc390b3d1fcbae
johnnybravo916 <johnong916@gmail.com>
2021-06-06 00:04:25 -0500

    added total deductions and net pay

--- zz-calculator/src/Total.jsx
+++ zz-calculator/src/Total.jsx
@@ -1,31 +1,43 @@
 import React from "react";
 
-const Total = () => {
+const Total = ({pay, net}) => {
     return (
         <div className="block-total">
             <div className="block-total__deductions mb-4">
                 <div className="row mb-1">
-                    <label
-                        htmlFor="daily_rate"
-                        className="col-sm-4 col-form-label"
-                    >
+                <label
+                    htmlFor="total_deductions"
+                    className="col-sm-4 col-form-label"
+                >
                         TOTAL DEDUCTIONS
                     </label>
                     <div className="col-sm-4">
-                        <input type="text" className="form-control" />
+                    <input
+                        className="form-control"
+                        name="total_deductions"
+                        value={pay.deductions}
+                        readOnly
+                        type="number"
+                    />
                     </div>
                 </div>
             </div>
             <div className="block-total__net mb-5">
                 <div className="row mb-1">
-                    <label
-                        htmlFor="daily_rate"
-                        className="col-sm-4 col-form-label"
-                    >
+                <label
+                    htmlFor="total_net_pay"
+                    className="col-sm-4 col-form-label"
+                >
                         TOTAL NET PAY
                     </label>
                     <div className="col-sm-4">
-                        <input type="text" className="form-control" />
+                    <input
+                        className="form-control"
+                        name="total_net_pay"
+                        value={net}
+                        readOnly
+                        type="number"
+                    />
                     </div>
                 </div>
             </div>
